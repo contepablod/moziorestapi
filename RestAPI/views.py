d2e013bc19bb7b8aa43e3ff414476b9f3bc0e48f
Pablo Conte <contepablod@gmail.com>
2022-05-25 18:03:36 -0300

    Creates Views for API

--- RestAPI/views.py
+++ RestAPI/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProviderSerializer, ServiceAreaSerializer
+from .models import Provider, ServiceArea
+
 
 # Create your views here.
+
+class ProviderViewSet(viewsets.ModelViewSet):
+    serializer_class = ProviderSerializer
+    queryset = Provider.objects.all()
+
+
+class ServiceAreaViewSet(viewsets.ModelViewSet):
+    queryset = ServiceArea.objects.all()
+    serializer_class = ServiceAreaSerializer
