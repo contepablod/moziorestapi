56a41ecc846cd9c69370c7171cf994386132c238
Pablo Conte <contepablod@gmail.com>
2022-05-25 17:57:16 -0300

    Creates Data Model

--- RestAPI/models.py
+++ RestAPI/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from djgeojson.fields import PointField
 
 # Create your models here.
+
+
+class Provider(models.Model):
+    Name = models.CharField(max_length=50)
+    Email = models.EmailField()
+    Phone = models.IntegerField()
+    Language = models.CharField(max_length=25)
+    Currency = models.CharField(max_length=10)
+
+
+class ServiceArea(models.Model):
+    Name = models.CharField(max_length=50)
+    Price = models.FloatField()
+    Location = PointField()
